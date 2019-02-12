c6e73b05f92190ed3ccf9aee099c25b64cd99318
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 20:48:14 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+  if block_given?
+    i = 0
+    while i < array.size
+      new_array << yield(array[i].even?)
+      i += 1
+    end
+    new_array
+  else
+    "Block not provided"
+  end
 end
